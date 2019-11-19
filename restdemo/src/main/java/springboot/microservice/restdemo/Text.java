0f42b06c74892854a7f8b6610ff8a727df78f4c7
kihong017 <kihong017@gmail.com>
2019-11-19 02:19:11 -0600

    - RESTFul API Methods Implementation.

--- restdemo/src/main/java/springboot/microservice/restdemo/Text.java
+++ restdemo/src/main/java/springboot/microservice/restdemo/Text.java
@@ -2,20 +2,43 @@ package springboot.microservice.restdemo;
 
 public class Text {
 
-    private long seq;
-    private String text;
+    private long id;
+    private String title;
+    private String content;
+    private String status;
+    private String author;
     
-	public long getSeq() {
-		return seq;
+	
+	public long getId() {
+		return id;
 	}
-	public void setSeq(long seq) {
-		this.seq = seq;
+	public void setId(long id) {
+		this.id = id;
 	}
-	public String getText() {
-		return text;
+	public String getTitle() {
+		return title;
 	}
-	public void setText(String text) {
-		this.text = text;
+	public void setTitle(String title) {
+		this.title = title;
 	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
     
 }
--- restdemo/src/main/java/springboot/microservice/restdemo/TextController.java
+++ restdemo/src/main/java/springboot/microservice/restdemo/TextController.java
@@ -1,46 +1,101 @@
 package springboot.microservice.restdemo;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TextController {
 	
-	@RequestMapping(value = "list", method = RequestMethod.GET)
-	public List<Text> test() {
-	    List<Text> list = new ArrayList<Text>();
-	     
-	    for(int i=1; i<=10; i++) {
-	    	Text helloWorld = new Text();
-	    	helloWorld.setSeq(i);
-	    	helloWorld.setText("HelloWorld");
-	             
-	        list.add(helloWorld);
-	    }
-	    return list;
+	private Map<Long, Text> textData = new HashMap<Long, Text>();
+	private Long nextId = (long) 0;
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String test() {
+	    
+		return textData.toString();
+		
     }
 	
-	@RequestMapping(value = "text/{seq}", method = RequestMethod.GET)
-	public Text detail(@PathVariable("seq") long seq) {
+	@RequestMapping(value = "/text/{id}", method = RequestMethod.GET)
+	public Text detail(@PathVariable("id") Long id) {
 
-		Text helloWorld = new Text();
-		helloWorld.setSeq(seq);
-		helloWorld.setText("HelloWorld");
-		
-		return helloWorld;
+		return textData.get(id);
 	}
 
+	
+//  Need to Change the RequestParam to RequestBody 	
+	@RequestMapping(value="/text", method= {RequestMethod.POST})
+	public String addText(@RequestParam(value="title")   String title
+						   , @RequestParam(value="content") String content
+						   , @RequestParam(value="status")  String status
+						   , @RequestParam(value="author")  String author) {
+		
+		Text newText = new Text();
+		
+		newText.setId(getNextId());
+		newText.setTitle(title);
+		newText.setContent(content);
+		newText.setStatus(status);
+		newText.setAuthor(author);
+		
+		textData.put(newText.getId(), newText);
+		
+		return "Add Successful";
+	}
+
+//  Need to Change the RequestParam to RequestBody
+	@RequestMapping(value="/text/{id}", method= {RequestMethod.PUT})
+	public Text modifyText(@PathVariable("id")            Long   id
+						 , @RequestParam(value="title")   String title
+						 , @RequestParam(value="content") String content
+						 , @RequestParam(value="status")  String status
+						 , @RequestParam(value="author")  String author) {
+		
+		Text modifyingText = textData.get(id);
+		
+		modifyingText.setTitle  (null != title ? title : modifyingText.getTitle());
+		modifyingText.setContent(null != content ? content : modifyingText.getContent());
+		modifyingText.setStatus (null != status ? status : modifyingText.getStatus());
+		modifyingText.setAuthor (null != author ? author : modifyingText.getAuthor());
+		
+		textData.put(id, modifyingText);
+		
+		return modifyingText;
+	}
+	
+	@RequestMapping(value="/text/{id}", method=RequestMethod.DELETE)
+	public String deleteText(@PathVariable("id") Long id) {
+
+		Text deleteText = textData.get(id);
+		
+		if (null != deleteText) {
+			
+			if (deleteText.getStatus().equals("publish")) {
+				deleteText.setStatus("trash");
+				textData.put(id, deleteText);
+			} else {
+				textData.remove(id);
+			}
+			
+		}
+		
+		return "Delete Successful";
+		
+	}
+	
+	
 	@RequestMapping(value = "test1", method = RequestMethod.GET)
 	public Text test1() {
 	    
 		Text helloWorld = new Text();
-		helloWorld.setSeq(0);
-		helloWorld.setText("test1");
+		helloWorld.setId(0);
+		helloWorld.setTitle("test1");
 		
 	    return helloWorld;
     }
@@ -49,19 +104,16 @@ public class TextController {
 	public Text test2() {
 
 		Text helloWorld = new Text();
-		helloWorld.setSeq(0);
-		helloWorld.setText("test2");
+		helloWorld.setId(0);
+		helloWorld.setTitle("test2");
 	    
 	    return helloWorld;
     }
 	
-	@RequestMapping(value = "test3", method = RequestMethod.GET)
-	public Text test3() {
+	public Long getNextId() {
 		
-		Text helloWorld = new Text();
-		helloWorld.setSeq(0);
-		helloWorld.setText("test3");
+		return nextId++;		
 		
-		return helloWorld;
 	}
+
 }
